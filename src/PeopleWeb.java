db7acc14ef7dc6c8f8d36438d7cb3b374c3ddc10
BrandenSandahl <branden157@gmail.com>
2016-02-24 13:46:05 -0500

    added read method in

--- src/PeopleWeb.java
+++ src/PeopleWeb.java
@@ -1,4 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PeopleWeb {
-    public static void main(String[] args) {
+
+
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        ArrayList<Person> peopleList = readFromCsv();
+
+        System.out.println("check it out dawg");
+
+
+    }
+
+
+
+    static ArrayList<Person> readFromCsv() throws FileNotFoundException {
+        File f = new File("people.csv");
+        Scanner s = new Scanner(f);
+
+        ArrayList<Person> peopleList = new ArrayList<>();
+
+        s.nextLine(); //skip a line
+
+        while (s.hasNext()) {
+            String[] lineSplit = s.nextLine().split(",");
+            Person p = new Person(Integer.parseInt(lineSplit[0]), lineSplit[1], lineSplit[2], lineSplit[3], lineSplit[4], lineSplit[5]);
+            peopleList.add(p);
+        }
+
+        return peopleList;
+
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,67 @@
+/**
+ * Created by branden on 2/24/16 at 13:31.
+ */
+public class Person {
+
+    private int id;
+    private String firstName, lastName, email, country, ip;
+
+
+    public Person(int id, String firstName, String lastName, String email, String country, String ip) {
+        setId(id);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+        setCountry(country);
+        setIp(ip);
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
